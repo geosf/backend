e5393a14ec0e5764f90fa9b997ac1c3aecccf7dd
Geovani Silva <54823653+geosf@users.noreply.github.com>
2019-10-17 09:15:05 -0300

    Add files via upload

---
+++ src/controllers/SpotController.js
@@ -0,0 +1,27 @@
+const User = require('../models/User');
+const Spot = require('../models/Spot');
+
+module.exports = {
+    async store(req, res){
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+        
+        const user = await User.findById(user_id);
+
+        if(!user){
+            return res.status(400).json({ error: 'User does not exist'});
+        }
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+
+        })
+
+        return res.json(spot);
+    }
+}
